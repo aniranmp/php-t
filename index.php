2429ada1a2b2bf16ac90ec2edec15d638f49151d
russell19990913 <hamrahiii@Hamrahs-Mac-mini.local>
2020-03-26 08:06:22 -0700

    test

--- index.php
+++ index.php
@@ -11,9 +11,13 @@
 		<h1>
 			<?php
 
-echo "Hello World";
-
-?>
+			echo "Hello World!";
+			$v1 = "this is a variable";
+			$v_int1 = 1;
+			$v_int2 = 2;
+			$v_intsum  = $v_int1 + $v_int2;
+			echo $v_intsum;
+			?>
 		</h1>
 	</center>
 

6bdf7700983009e7ff2c1a3d07a2d3707f3cef62
russell19990913 <hamrahiii@Hamrahs-Mac-mini.local>
2020-03-26 04:49:14 -0700

    little change

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+
+</html>
+
+<body>
+	<center>
+		<h1>
+			<?php
 
 echo "Hello World";
 
 ?>
+		</h1>
+	</center>
+
+</body>
+

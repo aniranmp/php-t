8450e91f3b6265e5bcd1c4ce7e031c8594c2aabb
russell19990913 <hamrahiii@Hamrahs-Mac-mini.local>
2020-03-29 05:10:57 -0700

    c

--- index.php
+++ index.php
@@ -11,19 +11,28 @@
 		<h1>
 			<?php
 
-			echo "Hello World!";
-			$v1 = "this is a variable";
-			$v_int1 = 1;
-			$v_int2 = 2;
-			$v_intsum  = $v_int1 + $v_int2;
+				echo "Hello World!";
+				$v1 = "this is a variable";
+				$v_int1 = 1;
+				$v_int2 = 2;
+				$v_intsum  = $v_int1 + $v_int2;
 
 			
 
 			?>
 			<br>
-			<?php
-			echo $v_intsum;
-			?>
+				<?php
+					echo $v_intsum;
+				?>
+			</br>
+			<br>
+				<?php
+					if($v_int1 == $v_int2){
+						echo "First and Second V are equal";
+					}else{
+						echo "the Input vars are not equal";
+					}
+				?>
 			</br>
 		</h1>
 	</center>
